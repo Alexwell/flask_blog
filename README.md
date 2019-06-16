ba00af089c078ccdc51d69af0403d51eea1b995c
Alexwell <alexl88v@gmail.com>
2019-06-16 23:30:28 +0300

    error logging added

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # $ export FLASK_APP=microblog.py
 # $ export FLASK_DEBUG=1
+
 #DB migration: 
 ##$ flask db migrate -m "name"
 ##$ flask db upgrade
+
+#mail error reports:
+# local mail server:
+## $ python -m smtpd -n -c DebuggingServer localhost:8025
+## export MAIL_SERVER=localhost
+## export MAIL_PORT=8025
+
+# gmail:
+## export MAIL_SERVER=smtp.googlemail.com
+## export MAIL_PORT=587
+## export MAIL_USE_TLS=1
+## export MAIL_USERNAME=test_mail@gmail.com
+## export MAIL_PASSWORD=pAsswoRd
+
--- config.py
+++ config.py
@@ -5,6 +5,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key-test'
+
+    # SQLAlchemy settings
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # error messages mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['alexlwebq@gmail.com']
